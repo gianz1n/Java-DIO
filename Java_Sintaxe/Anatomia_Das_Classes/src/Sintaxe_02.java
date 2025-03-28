9a00e53bfb8cd2fab9d24a72d639f781b00d33a1
Gianz1n <contato.gianofc@gmail.com>
2025-03-28 16:15:41 -0300

    nome e sobrenome

---
+++ Java_Sintaxe/Anatomia_Das_Classes/src/Sintaxe_02.java
@@ -0,0 +1,14 @@
+public class Sintaxe_02 {
+    public static void main(String[] args) {
+        String primeiroNome = "Gian";
+        String segundoNome = "Emanuel";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.print (nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
