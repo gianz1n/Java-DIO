741057bb96df6e8187510e7a104887d3c4247f73
Gianz1n <contato.gianofc@gmail.com>
2025-04-02 14:55:01 -0300

    Update

--- Java_Sintaxe/Anatomia_Das_Classes/src/edu.gian.segundasemana/Tipos_e_Variaveis.java
+++ Java_Sintaxe/Anatomia_Das_Classes/src/edu.gian.segundasemana/Tipos_e_Variaveis.java
@@ -2,12 +2,18 @@
 public class Tipos_e_Variaveis {
     public static void main(String[] args) {
 
+        // Variáveis
         byte idade = 123;
         short ano = 2004;
         int cep = 37550000; //se começar com 0 deverá ter outra tipagem
         long cpf = 00000000000L; //se começar com 0 deverá ter outra tipagem
         float pi = 3.14F;
         double salario = 1512.96;
+
+        // Constantes
+        final double VALOR_PI = 3.14; //Esse valor não poderá ser alterado
+
+        System.out.print(VALOR_PI);
     }
     
 }
