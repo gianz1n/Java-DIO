8a87f7799e2a544a15b7197f02ac973d8e8d36d0
Gianz1n <contato.gianofc@gmail.com>
2025-04-02 14:55:10 -0300

    Operadores

---
+++ Java_Sintaxe/Anatomia_Das_Classes/src/edu.gian.segundasemana/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        String nome = "Gian"; // = atribuição
+        int soma = 1 + 1; // + aritmético
+        int subtracao = 1 - 1; // + aritmético
+        int multiplicacao = 1 * 1; // + aritmético
+        double divisao = 1 / 1; // + aritmético
+        int restoDiv = 1 % 1; // + aritmético
+
+        //Concatenação
+        String nomeCompleto = "Gian" + " Emanuel";
+        System.out.print(nomeCompleto);
+    }
+}
